1da5586a61dfeadc7ce69681fed2289cf852b646
Ross <49296963+diceyecid@users.noreply.github.com>
2022-07-13 16:00:41 -0700

    added template explore page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExploreComponent } from './page/explore/explore.component';
 import { LoginComponent } from './page/login/login.component';
 
 const routes: Routes = 
 [
+	{ path: '', component: ExploreComponent },
 	{ path: 'login', component: LoginComponent },
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { LoginComponent } from './page/login/login.component'; 
+import { LoginComponent } from './page/login/login.component';
+import { ExploreComponent } from './page/explore/explore.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
