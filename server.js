8b23312242e06ebaec20b050ba2f6eb31718af2d
Your name here <your_email@example.com>
2022-07-15 15:02:10 -0700

    Mongodb connection working succesfully

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "https://data.mongodb-api.com/app/data-udxjr/endpoint/data/v1"
-);
-var db = mongoose.connection;
-
 const app = express();
 port = 3081;
 
 const cardsUsers = [];
+
+app.get("/", (req, res) => {
+  res.send("App Works!");
+});
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
+mongoose.connect(
+  "mongodb+srv://ash:finalproject@cluster0.ilz0p.mongodb.net/?retryWrites=true&w=majority"
+);
+var db = mongoose.connection;
+db.on("connected", () => {
+  console.log("Connected");
+});
