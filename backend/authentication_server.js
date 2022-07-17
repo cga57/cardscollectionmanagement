b92b9627ef84fa6f1991d64b83b18d31b695aea6
Your name here <your_email@example.com>
2022-07-17 02:51:30 -0700

    Update on authentication

--- backend/authentication_server.js
+++ backend/authentication_server.js
@@ -2,3 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
+
+// used in class example to help code this
+
+var session = require("express-session");
+
+app.use(
+  session({
+    name: "session",
+    secret: "qqq",
+    resave: false,
+    maxAge: 60 * 60 * 1000, // 60 minutes
+  })
+);
+
+app.get("/session1", (req, res) => {
+  // ask db if this is a valid applicationUser based on req.body
+  applicationUser = req.body;
+  // searh database to see if value exist
+
+  if (applicationUser) {
+    // valid login
+    req.session.usr = applicationUser;
+    res.send(`<a href="/session2"> NEXT PAGE </a>`);
+  } else {
+    // invalid - redirect them to login again TODO: write login logic
+  }
+});
