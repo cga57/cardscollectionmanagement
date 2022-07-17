48c26f6a106a88f0b3a08593156e494854f77815
Ross <49296963+diceyecid@users.noreply.github.com>
2022-07-17 01:04:36 -0700

    mocked a set of sample deck with model

--- frontend/src/app/component/carousel3d/carousel3d.component.ts
+++ frontend/src/app/component/carousel3d/carousel3d.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Deck } from 'src/app/model/deck';
 
 @Component({
 	selector: 'app-carousel3d',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class Carousel3dComponent implements OnInit 
 {
-	@Input() slides : { [ key : string ] : string }[] = [];
+	@Input() slides : Deck[] = [];
 	activeIndex = 0;
 
 	constructor() { }
--- frontend/src/app/model/sampleDecks.ts
+++ frontend/src/app/model/sampleDecks.ts
@@ -1,12 +1,85 @@
-export const sampleDecks= 
+import { Brand } from 'src/app/model/brand';
+import { Deck } from 'src/app/model/deck';
+
+const fontaine : Brand =
+{
+	_id: '0',
+	name: 'Fontaine',
+	website: 'http://www.fontainecards.com',
+};
+
+export const sampleDecks : Deck[] = 
 [
-	{ name: 'Blue Fontaine', image: 'assets/sample_decks/fontaine_blue.png' },
-	{ name: 'Black Fontaine', image: 'assets/sample_decks/fontaine_black.png' },
-	{ name: 'Pink Fontaine', image: 'assets/sample_decks/fontaine_pink.png' },
-	{ name: 'Green Fontaine', image: 'assets/sample_decks/fontaine_green.png' },
-	{ name: 'Slight Fontaine', image: 'assets/sample_decks/fontaine_slight.png' },
-	{ name: 'Carrot V1 Fontaine', image: 'assets/sample_decks/fontaine_carrot_v1.png' },
-	{ name: 'Chinatown Fontaine', image: 'assets/sample_decks/fontaine_chinatown.png' },
-	{ name: 'Wine Fontaine', image: 'assets/sample_decks/fontaine_wine.png' },
-	{ name: 'Guess V1 Fontaine', image: 'assets/sample_decks/fontaine_guess_v1.png' },
+	{
+		_id: '0',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Blue',
+		image: 'assets/sample_decks/fontaine_blue.png',
+		brand: fontaine,
+	},
+	{
+		_id: '1',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Black',
+		image: 'assets/sample_decks/fontaine_black.png', 
+		brand: fontaine,
+	},
+	{
+		_id: '2',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Pink',
+		image: 'assets/sample_decks/fontaine_pink.png',
+		brand: fontaine,
+	},
+	{
+		_id: '3',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Green',
+		image: 'assets/sample_decks/fontaine_green.png', 
+		brand: fontaine,
+	},
+	{
+		_id: '4',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Slight',
+		image: 'assets/sample_decks/fontaine_slight.png', 
+		brand: fontaine,
+	},
+	{
+		_id: '5',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Carrot V1',
+		image: 'assets/sample_decks/fontaine_carrot_v1.png',
+		brand: fontaine,
+	},
+	{
+		_id: '6',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Chinatown',
+		image: 'assets/sample_decks/fontaine_chinatown.png',
+		brand: fontaine,
+	},
+	{
+		_id: '7',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Wine',
+		image: 'assets/sample_decks/fontaine_wine.png',
+		brand: fontaine,
+	},
+	{
+		_id: '8',
+		isPublic: false, 
+		name: 'Fontaine Playing Cards', 
+		edition: 'Guess V1',
+		image: 'assets/sample_decks/fontaine_guess_v1.png',
+		brand: fontaine,
+	},
 ];
