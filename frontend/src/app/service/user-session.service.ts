963acd8b35bae6bd326ca7a18f62ffcd2f74290c
Jason Sahota <109596802+jss38@users.noreply.github.com>
2022-08-02 03:15:44 -0700

    Add _id attribute to UserDeck, with user email from the user-session service

---
+++ frontend/src/app/service/user-session.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserSessionService {
+
+  // Default values
+  email: string = "null";
+  loggedIn: boolean = false;
+
+  constructor() { }
+
+  getEmail(): string {
+    return this.email;
+  }
+  
+  getLoggedInStatus(): boolean {
+    return this.loggedIn;
+  }
+
+  setEmail(email: string) {
+    this.email = email;
+    this.loggedIn = true;
+  }
+}
